f3c1b21bf9c4e1101841fe27cbaeedc94fd7fcab
Karla Rodrigues <karlaadautor@gmail.com>
2020-10-20 17:42:46 -0400

    Updates

---
+++ precache-manifest.3936c213a45bbd860ab146979156a676.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4192ea97c3f4359d5ae677534a7ae314",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5394da0c6a57915a584",
+    "url": "/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "425a20945e719e96a607",
+    "url": "/static/css/main.8fb19b3d.chunk.css"
+  },
+  {
+    "revision": "a5394da0c6a57915a584",
+    "url": "/static/js/2.3ed5bcb3.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.3ed5bcb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "425a20945e719e96a607",
+    "url": "/static/js/main.9866fd62.chunk.js"
+  },
+  {
+    "revision": "9a2957c89f461be10c84",
+    "url": "/static/js/runtime-main.2460fb22.js"
+  },
+  {
+    "revision": "5d03ceb5e71899fee705669b303ba364",
+    "url": "/static/media/banner.5d03ceb5.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ShoppingList/precache-manifest.0004dd8b68db3035a9ae400b0ac65bdb.js"
+  "/precache-manifest.3936c213a45bbd860ab146979156a676.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ShoppingList/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
