89f8b4f28bda70dd8e644fafe1b7ca228c5c8179
Karla Rodrigues <karlaadautor@gmail.com>
2020-10-20 15:58:51 -0400

    Updates

---
+++ precache-manifest.0004dd8b68db3035a9ae400b0ac65bdb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2810586c174aec99842e46d73a2f597f",
+    "url": "/ShoppingList/index.html"
+  },
+  {
+    "revision": "a5394da0c6a57915a584",
+    "url": "/ShoppingList/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "4006523e028c3a844843",
+    "url": "/ShoppingList/static/css/main.c3965e43.chunk.css"
+  },
+  {
+    "revision": "a5394da0c6a57915a584",
+    "url": "/ShoppingList/static/js/2.3ed5bcb3.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ShoppingList/static/js/2.3ed5bcb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4006523e028c3a844843",
+    "url": "/ShoppingList/static/js/main.93379b31.chunk.js"
+  },
+  {
+    "revision": "180722bebb1dc435fbc0",
+    "url": "/ShoppingList/static/js/runtime-main.969a9357.js"
+  },
+  {
+    "revision": "5d03ceb5e71899fee705669b303ba364",
+    "url": "/ShoppingList/static/media/banner.5d03ceb5.jpg"
+  }
+]);
